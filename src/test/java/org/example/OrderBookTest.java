0f4a5a2647ffcd8a90dceb85e6985c7af3bb9a82
hastishb <hasti.sahabi@tapsi.cab>
2023-07-25 13:44:48 +0330

    added test for orderbook priority queues

--- src/test/java/org/example/OrderBookTest.java
+++ src/test/java/org/example/OrderBookTest.java
@@ -8,6 +8,9 @@ import org.example.repository.TradeRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -229,7 +232,7 @@ public class OrderBookTest {
     }
 
     @Test
-    void test_2() {
+    void test() {
         OrderBook orderBook = OrderBook.getOrderBook();
         orderBook.place(new Order("1,S,100,100"));
         orderBook.place(new Order("2,S,100,200"));
@@ -253,5 +256,51 @@ public class OrderBookTest {
         assertThat(orderBook.getBids().size()).isEqualTo(0);
     }
 
-    // TEST ADDING
+    @Test
+    void givenEmptyOrderBook_whenPlaceBidOrders_TheCorrectPriority() {
+        //given
+        OrderBook orderBook = OrderBook.getOrderBook();
+
+        //when
+        orderBook.place(new Order("1,B,100,100"));
+        orderBook.place(new Order("2,B,99,100"));
+        orderBook.place(new Order("3,B,102,100"));
+        orderBook.place(new Order("4,B,100,100"));
+        orderBook.place(new Order("5,B,97,100"));
+        orderBook.place(new Order("6,B,105,100"));
+
+        //then
+        var bids = new PriorityQueue<>(orderBook.getBids());
+        assertThat(bids.poll().getId()).isEqualTo(6);
+        assertThat(bids.poll().getId()).isEqualTo(3);
+        assertThat(bids.poll().getId()).isEqualTo(1);
+        assertThat(bids.poll().getId()).isEqualTo(4);
+        assertThat(bids.poll().getId()).isEqualTo(2);
+        assertThat(bids.poll().getId()).isEqualTo(5);
+        assertThat(bids.poll()).isNull();
+    }
+
+    @Test
+    void givenEmptyOrderBook_whenPlaceAskOrders_TheCorrectPriority() {
+        //given
+        OrderBook orderBook = OrderBook.getOrderBook();
+
+        //when
+        orderBook.place(new Order("1,S,100,100"));
+        orderBook.place(new Order("2,S,99,100"));
+        orderBook.place(new Order("3,S,102,100"));
+        orderBook.place(new Order("4,S,100,100"));
+        orderBook.place(new Order("5,S,97,100"));
+        orderBook.place(new Order("6,S,105,100"));
+
+        //then
+        var asks = new PriorityQueue<>(orderBook.getAsks());
+        assertThat(asks.poll().getId()).isEqualTo(5);
+        assertThat(asks.poll().getId()).isEqualTo(2);
+        assertThat(asks.poll().getId()).isEqualTo(1);
+        assertThat(asks.poll().getId()).isEqualTo(4);
+        assertThat(asks.poll().getId()).isEqualTo(3);
+        assertThat(asks.poll().getId()).isEqualTo(6);
+        assertThat(asks.poll()).isNull();
+    }
 }
